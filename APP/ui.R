41b986bb965768ef051d6208c7b31329cb70235b
zhangyan715 <635953297@qq.com>
2015-05-29 11:42:35 +0800

    APP2 with some weight

--- APP/ui.R
+++ APP/ui.R
@@ -1,24 +1,31 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Hello World!"),
-  
-  # Sidebar with a slider input for the number of bins
+  titlePanel(strong("My Shiny App")),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 10,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
+                 h2(strong('Installation')),
+                 p(h5('Shiny is available on CRAN,so you can install it in the usual way from your R console:')),
+                 code('install.packages("shiny")'),
+                 br(),
+                 br(),
+                 br(),
+                 br(),
+                 img(src='r.png',height=50,weight=50),
+                 'chiny is a product of',
+                 span('RStudio',style='color:blue')
+                 ),
     mainPanel(
-      plotOutput("distPlot")
+      br(),
+      h1(strong('introducing Shiny')),
+      p('Shiny is a new package from RStudio that makes it ',em('incredibly easy'),'to build interactive web applications with R.'),
+      br(),
+      p('For an introduction and live examples,visit the ',a("Shiny homepage.", 
+                                                             href = "http://www.rstudio.com/shiny")),
+      br(),
+      h1(strong('Feartures')),
+      tags$ul(tags$li('Build useful....'),tags$li('Shiny'))
     )
   )
-))
+)
+)
--- APP2/server.R
+++ APP2/server.R
@@ -2,5 +2,5 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
+  
 })
